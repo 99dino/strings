255885a18f3b0c0f6eb4de1f3675df10c9a6aeb4
priyanshu <priyanshumaurya2810@gmail.com>
2023-10-08 23:00:33 +0530

    Lab work putting on github

---
+++ app/src/main/java/com/example/strings/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.strings;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import java.util.*;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
